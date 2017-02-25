6f8dae57acd940b0c7fa287a3a15f5705a68292b
Kenji Ohtsuka <kok.fdcm@gmail.com>
2017-02-25 22:59:03 +0900

    base

---
+++ test.pyx
@@ -0,0 +1,22 @@
+class TestCase:
+    def __init__(self, name):
+        self.name = name
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.wasRun = None
+        TestCase.__init__(self, name)
+
+    def testMethod(self):
+        self.wasRun = 1
+
+    def run(self):
+        method = getattr(self, self.name)
+        method()
+
+
+
+test = WasRun("testMethod")
+print(test.wasRun)
+test.run()
+print(test.wasRun)
