7b646876d34f110474faf530618cf02f9dfe6d1c
Kenji Ohtsuka <kok.fdcm@gmail.com>
2017-02-25 23:06:48 +0900

    update test

--- test.pyx
+++ test.pyx
@@ -2,6 +2,11 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
+    def run(self):
+        method = getattr(self, self.name)
+        method()
+
+
 class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
@@ -10,13 +15,13 @@ class WasRun(TestCase):
     def testMethod(self):
         self.wasRun = 1
 
-    def run(self):
-        method = getattr(self, self.name)
-        method()
+
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test = WasRun("testMethod")
+        assert(not test.wasRun)
+        test.run()
+        assert(test.wasRun)
 
 
-
-test = WasRun("testMethod")
-print(test.wasRun)
-test.run()
-print(test.wasRun)
+TestCaseTest("testRunning").run()
