d59ca0e21e59964239ed2e3657060673b7fcb41a
Kenji Ohtsuka <kok.fdcm@gmail.com>
2017-02-27 21:50:10 +0900

    feat

--- test.pyx
+++ test.pyx
@@ -5,10 +5,15 @@ class TestCase:
     def setUp(self):
         pass
 
+    def tearDown(self):
+        pass
+
     def run(self):
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
 
 
 class WasRun(TestCase):
@@ -17,26 +22,32 @@ class WasRun(TestCase):
         TestCase.__init__(self, name)
 
     def setUp(self):
-        self.wasRun = None
-        self.wasSetUp = 1
         self.log = "setUp "
 
     def testMethod(self):
-        self.wasRun = 1
+        self.log = self.log + "testMethod "
+
+    def tearDown(self):
+        self.log = self.log + "tearDown "
 
 
 class TestCaseTest(TestCase):
     def setUp(self):
-        self.test = WasRun("testMethod")
+        self.test = WasRun("testMethod ")
 
     def testSetUp(self):
-        test.run()
+        self.test.run()
         assert(self.test.wasSetUp)
-        assert("setUp " == self.test.log)
+        assert("setUp testMethod " == self.test.log)
 
     def testRunning(self):
         self.test.run()
         assert(self.test.wasRun)
+
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod tearDown " == test.log)
 
 
 TestCaseTest("testRunning").run()
